af25a3659c1497e94b2f407823b17c13790055d0
S.J.R. van Schaik <stephan@synkhronix.com>
2019-05-18 04:45:49 +0200

    implement known list of CPUs affected by Meltdown

--- source/apple/meltdown.c
+++ source/apple/meltdown.c
@@ -7,9 +7,14 @@
 #include <info/cpuid.h>
 
 void
+cpu_query_meltdown_info(struct meltdown_info *info);
+
+void
 query_meltdown_info(struct meltdown_info *info)
 {
 	memset(info, 0, sizeof *info);
+
+	cpu_query_meltdown_info(info);
 
 	info->has_pcid = cpuid_has_feature("pcid");
 	info->has_invpcid = cpuid_has_feature("invpcid");
