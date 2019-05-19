71a62b3ef172b06d632c0365c98562b527805f38
S.J.R. van Schaik <stephan@synkhronix.com>
2019-05-19 19:26:23 +0200

    check Mac OS X support for double map and SSBD

--- source/apple/meltdown.c
+++ source/apple/meltdown.c
@@ -6,16 +6,33 @@
 
 #include <info/cpuid.h>
 
+#include "os.h"
+
 void
 cpu_query_meltdown_info(struct meltdown_info *info);
 
 void
 query_meltdown_info(struct meltdown_info *info)
 {
+	int major, minor, patch;
+
 	memset(info, 0, sizeof *info);
 
 	cpu_query_meltdown_info(info);
 
 	info->has_pcid = cpuid_has_feature("pcid");
 	info->has_invpcid = cpuid_has_feature("invpcid");
+
+	major = get_os_version(VERSION_MAJOR);
+	minor = get_os_version(VERSION_MINOR);
+	patch = get_os_version(VERSION_PATCH);
+
+	if (major == 10 &&
+		((minor == 11 && patch >= 6) ||
+		 (minor == 12 && patch >= 6) ||
+		 (minor == 13 && patch >= 2) ||
+		  minor >= 14)) {
+		info->kpti_present = 1;
+		info->kpti_enabled = info->affected;
+	}
 }
--- source/apple/ssb.c
+++ source/apple/ssb.c
@@ -6,9 +6,13 @@
 
 #include <vuln/ssb.h>
 
+#include "os.h"
+
 void
 query_ssb_info(struct ssb_info *info)
 {
+	int major, minor, patch;
+
 	memset(info, 0, sizeof *info);
 
 	info->affected = 1;
@@ -18,4 +22,14 @@ query_ssb_info(struct ssb_info *info)
 
 	if (cpuid_has_feature("no_ssb"))
 		info->affected = 0;
+
+	major = get_os_version(VERSION_MAJOR);
+	minor = get_os_version(VERSION_MINOR);
+	patch = get_os_version(VERSION_PATCH);
+
+	if (major == 10 &&
+		((minor == 13 && patch >= 6) ||
+		  minor >= 14)) {
+		info->ssbd = SSBD_OS_SUPPORT;
+	}
 }
